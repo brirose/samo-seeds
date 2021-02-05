485ef2f625a85f664f46214256cb4930c8e8897e
brirose <brirose.baker@gmail.com>
2021-02-05 14:28:06 -0800

    widgit overview

--- samo-seeds/app.R
+++ samo-seeds/app.R
@@ -4,37 +4,85 @@
 
 library(tidyverse)
 library(shiny)
-library(here)
+library(shinythemes)
 
 ### define data etc once its clean
 
 
 
 # Define UI for application
-ui <- fluidPage(
-    
-    navbarPage("SAMO Seeds",
-               tabPanel("Overview"
-                   
-               ),
-               tabPanel("Flora",
-                        sidebarLayout(
-                            sidebarPanel("widgets",
-                                        # PUT WIDGETS HERE
-                            ),
-                            mainPanel("output",
-                                      plotOutput("HERE"))
-                        )
-               ),
-               tabPanel("Phenology"),
-               tabPanel("Map of Locations"),
-               tabPanel("Herbarium Label")
-               
+ui <- fluidPage(theme = shinytheme("flatly"), # theme may change if I have time :)
+                
+                navbarPage("SAMO Seeds",
+                           tabPanel("Overview"
+                                    
+                           ),
+                           tabPanel("Flora",
+                                    sidebarLayout(
+                                        sidebarPanel("Choose Taxon",
+                                                     selectInput("select", label = h3("Select box"), 
+                                                                 choices = list("Choice 1" = 1, 
+                                                                                "Choice 2" = 2, 
+                                                                                "Choice 3" = 3), 
+                                                                 selected = 1)
+                                        ),
+                                        mainPanel("output",
+                                                  plotOutput("HERE")
+                                        )
+                                    )
+                           ),
+                           
+                           tabPanel("Phenology",
+                                    sidebarLayout(
+                                        sidebarPanel("widgets",
+                                                     
+                                                     selectInput("select", label = h3("Select box"), 
+                                                                 choices = list("Choice 1" = 1, 
+                                                                                "Choice 2" = 2, 
+                                                                                "Choice 3" = 3), 
+                                                                 selected = 1),
+                                                     radioButtons("radio", label = h3("Radio buttons"),
+                                                                  choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3), 
+                                                                  selected = 1)
+                                                     
+                                        ),
+                                        mainPanel("output",
+                                                  plotOutput("HERE"))
+                                    )
+                           ),
+                           
+                           tabPanel("Map of Locations",
+                                    sidebarLayout(
+                                        sidebarPanel("widgets",
+                                                     checkboxGroupInput("checkGroup", 
+                                                                        label = h3("Checkbox group"), 
+                                                                        choices = list("Choice 1" = 1, 
+                                                                                       "Choice 2" = 2, 
+                                                                                       "Choice 3" = 3),
+                                                                        selected = 1)
+                                        ),
+                                        mainPanel("output",
+                                                  plotOutput("HERE"))
+                                        )
+                                    ),
+                           tabPanel("Herbarium Label",
+                                    sidebarLayout(
+                               sidebarPanel("widgets",
+                                            textInput("text", 
+                                                      label = h3("Text input"), 
+                                                      value = "Enter text..."), # PUT WIDGETS HERE
+                               ),
+                               mainPanel("output",
+                                         plotOutput("HERE"))
+                               )
+                           )
+                           
                
     )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic
+
 server <- function(input, output) {
 
     ##Need to clean data to use it lol
